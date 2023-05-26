624b6d97e211d1494501ab84f01c2b91c078a6fa
Ver3Parfait <militellolucas@icloud.com>
2023-05-26 15:28:20 +0200

    Boilerplate

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import {
+  MD3LightTheme as DefaultTheme,
+  PaperProvider,
+} from "react-native-paper";
+import { useTheme } from "react-native-paper";
+import { View, Text, useColorScheme } from "react-native";
 
-export default function App() {
+import { default as colorsLight } from "./colors_light.json";
+import { default as colorsDark } from "./colors_dark.json";
+
+function PaymentScreen() {
+  const theme = useTheme();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: theme.colors.background,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Hello</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  const colorScheme = useColorScheme();
+  const paperTheme =
+    colorScheme === "dark"
+      ? { ...DefaultTheme, ...colorsDark }
+      : { ...DefaultTheme, ...colorsLight };
+  return (
+    <PaperProvider theme={paperTheme}>
+      <PaymentScreen />
+    </PaperProvider>
+  );
+}
--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,14 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      "module:react-native-dotenv",
+      "react-native-reanimated/plugin"
+    ],
+    env: {
+      production: {
+        plugins: ['react-native-paper/babel'],
+      },
+    },
   };
 };
