085005c095262476211812f35483d33ba85fd468
Ver3Parfait <militellolucas@icloud.com>
2023-05-31 16:27:02 +0200

    Ajout des composant de style

--- src/screens/Profile/ProfileScreen.jsx
+++ src/screens/Profile/ProfileScreen.jsx
@@ -1,13 +1,7 @@
 import React, { useState } from "react";
-import { View, StyleSheet, ScrollView } from "react-native";
-import {
-  Text,
-  Avatar,
-  Title,
-  Caption,
-  List,
-  TouchableRipple,
-} from "react-native-paper";
+import {  StyleSheet, ScrollView } from "react-native";
+import { Avatar, Title, Caption, List, Surface } from "react-native-paper";
+import { useTheme } from "react-native-paper";
 
 export default function ProfileScreen({navigation}) {
   const [expanded, setExpanded] = useState(false);
@@ -16,22 +10,24 @@ export default function ProfileScreen({navigation}) {
     setExpanded(!expanded);
   };
 
+  const theme = useTheme();
+  const styles = getStyles(theme);
   return (
-    <View style={styles.container}>
-      <View style={styles.userInfoSection}>
-        <View style={styles.userInfo}>
+    <Surface style={styles.container}>
+      <Surface style={styles.userInfoSection}>
+        <Surface style={styles.userInfo}>
           <Avatar.Image
             source={{
               uri: "https://i.pinimg.com/236x/6f/24/37/6f24371638c703bd61d3c67dc51762e1.jpg",
             }}
             size={80}
           />
-          <View style={styles.userDetails}>
+          <Surface style={styles.userDetails}>
             <Title style={styles.title}>Militello Lucas</Title>
             <Caption style={styles.caption}>Apprenti</Caption>
-          </View>
-        </View>
-      </View>
+          </Surface>
+        </Surface>
+      </Surface>
 
       <List.Section>
         <List.Accordion
@@ -43,7 +39,7 @@ export default function ProfileScreen({navigation}) {
           expanded={expanded}
           onPress={handleAccordionToggle}
         >
-          <List.Item title="Nom: Militello Lucas" />
+          <List.Item style={styles.items} title="Nom: Militello Lucas" />
           <List.Item title="Téléphone: 07 69 49 56 46" />
           <List.Item title="Email: militellolucas@icloud.com" />
           <List.Item title="Adresse: 123 rue du Paradis" />
@@ -51,19 +47,19 @@ export default function ProfileScreen({navigation}) {
         </List.Accordion>
       </List.Section>
 
-      <View style={styles.section}>
+      <Surface style={styles.section}>
         <Title style={styles.sectionTitle}>Cours</Title>
-        <View style={styles.infoBoxWrapper}>
-          <View style={styles.infoBox}>
+        <Surface style={styles.infoBoxWrapper}>
+          <Surface style={styles.infoBox}>
             <Title style={styles.infoBoxTitle}>90</Title>
             <Caption style={styles.infoBoxCaption}>En cours</Caption>
-          </View>
-          <View style={[styles.infoBox, styles.infoBoxSeparator]}>
+          </Surface>
+          <Surface style={[styles.infoBox, styles.infoBoxSeparator]}>
             <Title style={styles.infoBoxTitle}>150</Title>
             <Caption style={styles.infoBoxCaption}>Terminés</Caption>
-          </View>
-        </View>
-      </View>
+          </Surface>
+        </Surface>
+      </Surface>
       <ScrollView>
         <List.Section>
           <List.AccordionGroup>
@@ -128,72 +124,78 @@ export default function ProfileScreen({navigation}) {
           </List.AccordionGroup>
         </List.Section>
       </ScrollView>
-    </View>
+    </Surface>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-  userInfoSection: {
-    paddingHorizontal: 30,
-    paddingBottom: 25,
-    borderBottomColor: "#e0e0e0",
-    borderBottomWidth: 1,
-  },
-  userInfo: {
-    flexDirection: "row",
-    marginTop: 15,
-  },
-  userDetails: {
-    marginLeft: 20,
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 5,
-  },
-  caption: {
-    fontSize: 14,
-    lineHeight: 14,
-    fontWeight: "500",
-  },
-  section: {
-    paddingHorizontal: 30,
-    paddingTop: 25,
-    paddingBottom: 15,
-  },
-  sectionTitle: {
-    fontSize: 18,
-    fontWeight: "bold",
-    marginBottom: 10,
-  },
-  infoBoxWrapper: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    height: 100,
-  },
-  infoBox: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    borderTopWidth: 1,
-    borderTopColor: "#e0e0e0",
-  },
-  infoBoxSeparator: {
-    borderLeftWidth: 1,
-    borderLeftColor: "#e0e0e0",
-  },
-  infoBoxTitle: {
-    fontSize: 24,
-    fontWeight: "bold",
-  },
-  infoBoxCaption: {
-    fontSize: 14,
-    lineHeight: 14,
-    fontWeight: "500",
-  },
-});
+const getStyles = (theme) => {
+  return StyleSheet.create({
+    items: {
+      backgroundColor:useTheme().colors.primary
+    },
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+    },
+    userInfoSection: {
+      paddingHorizontal: 30,
+      paddingBottom: 25,
+      borderBottomColor: "#e0e0e0",
+      borderBottomWidth: 1,
+    },
+    userInfo: {
+      flexDirection: "row",
+      marginTop: 15,
+    },
+    userDetails: {
+      marginLeft: 20,
+      justifyContent: "center",
+    },
+    title: {
+      fontSize: 24,
+      fontWeight: "bold",
+      marginBottom: 5,
+    },
+    caption: {
+      fontSize: 14,
+      lineHeight: 14,
+      fontWeight: "500",
+    },
+    section: {
+      paddingHorizontal: 30,
+      paddingTop: 25,
+      paddingBottom: 15,
+    },
+    sectionTitle: {
+      fontSize: 18,
+      fontWeight: "bold",
+      marginBottom: 10,
+    },
+    infoBoxWrapper: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      height: 100,
+    },
+    infoBox: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+      borderTopWidth: 1,
+      borderTopColor: "#e0e0e0",
+    },
+    infoBoxSeparator: {
+      borderLeftWidth: 1,
+      borderLeftColor: "#e0e0e0",
+    },
+    infoBoxTitle: {
+      fontSize: 24,
+      fontWeight: "bold",
+    },
+    infoBoxCaption: {
+      fontSize: 14,
+      lineHeight: 14,
+      fontWeight: "500",
+    },
+  });
+  
+}
