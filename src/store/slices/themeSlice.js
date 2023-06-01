6b19da9581f8dbc6299636756f50de860fe474f5
Ver3Parfait <militellolucas@icloud.com>
2023-06-01 10:45:33 +0200

    réglage des bugs

--- src/store/slices/themeSlice.js
+++ src/store/slices/themeSlice.js
@@ -1,13 +1,17 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createSlice } from "@reduxjs/toolkit";
+
+
 
 const themeSlice = createSlice({
   name: "theme",
   initialState: {
-    theme:"dark",
+    theme:AsyncStorage.getItem('theme') || 'light',
   },
   reducers: {
     toggleTheme: (state,action) => {       
       state.theme = state.theme === "light" ? 'dark' : "light"
+      AsyncStorage.setItem('theme', state.theme) 
     },
   },
 });
